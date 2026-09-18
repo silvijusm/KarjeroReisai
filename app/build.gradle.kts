c2b604d67f5293f27d14ba13883ff978cb0cd0e2
silvijusm <silvijusm@gmail.com>
2026-09-19 01:00:06 +0300

    Update Compose BOM version to 2026.08.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
+    val composeBom = platform("androidx.compose:compose-bom:2026.08.01")
 
     implementation(composeBom)
     androidTestImplementation(composeBom)
