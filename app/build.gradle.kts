2bc7a51a8d2f2bbe63710c7e7c63a16ca92ccbb6
silvijusm <silvijusm@gmail.com>
2026-09-20 14:03:17 +0300

    Bump app version to 0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "lt.karjeroreisai.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.6.1"
     }
 
     buildFeatures {
